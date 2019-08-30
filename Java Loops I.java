cef4598d5d4e3e264d2022966657609ec3691250
Shubhamag19 <33371098+Shubhamag19@users.noreply.github.com>
2019-08-30 20:13:12 +0530

    Update Java Loops I.java

--- Java Loops I.java
+++ Java Loops I.java
@@ -8,8 +8,6 @@ import java.util.regex.*;
 
 public class Solution {
 
-
-
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -21,5 +19,7 @@ public class Solution {
         for(int i=1;i<=10;i++){
             int res=N*i;
             System.out.println(N+" "+"x"+" "+i+" "+"="+" "+res);
-    }}}
+        }
+        }
+    }
 }
